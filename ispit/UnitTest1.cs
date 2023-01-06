ae01636c139991dab7b592cf2177373f3f6d6d47
38164 <branislav.jovanovic@gmail.com>
2023-01-06 17:57:37 +0100

    Add project files.

---
+++ ispit/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace ispit
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
